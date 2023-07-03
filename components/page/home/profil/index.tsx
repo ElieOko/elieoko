51032b04f86b9ef71c3199032331351fbe6a1186
Elie Oko <elieoko100@gmail.com>
2023-07-03 09:53:58 +0100

    build:v2 profilComponent in homeComponent

--- components/page/home/profil/index.tsx
+++ components/page/home/profil/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@material-tailwind/react";
+
 export default function ProfilComponent(){
     const profil_text : string = `Ma mission c'est de donner de la joie à tous
     ces enfants africains qui rêve d'avoir 
@@ -10,12 +12,12 @@ export default function ProfilComponent(){
         <>
             <section className={`h-full flex flex-col justify-center px-4 2xl:px-72 xl:px-56 lg:px-32  md:px-28 sm:px-8 py-32 sm:py-52`}>
                 <div>
-                   <h2 className={`font-semibold text-sky-500`}>Salut, j'ai pour nom </h2>
+                   <h2 className={`font-semibold text-[#858ea6]`}>Salut, je m'appelle </h2>
                 </div>
                 <section className={``}>
-                    <h1 className={`text-gray-300 font-bold text-3xl lg:text-7xl sm:text-5xl md:text-6xl mt-4`}>Elie Oko</h1>
+                    <h1 className={`text-gray-300 font-bold text-3xl lg:text-7xl sm:text-5xl md:text-6xl mt-4`}> <span className="text-[#1b4be8]">E</span>lie <span className="text-[#d61212]">O</span>ko</h1>
                     <div>
-                        <span>Je suis <i>{our_responsibilities[1]}</i>  </span>
+                        <span className="text-gray-400">Je suis <i>{our_responsibilities[1]}</i>  </span>
                     </div>
                 </section>
                 <section className={``}>
@@ -23,7 +25,15 @@ export default function ProfilComponent(){
                         <p className={`text-gray-400  text-sm md:text-lg sm:text-md mt-10 tracking-wider`}>{profil_text}</p>
                     </blockquote>
                 </section>
-                
+                <div>
+                    <a href="#" className="text-[#fff]"
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 animate-bounce-bottom">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+                    </svg>
+                    </a>
+                    
+                </div>
             </section>
         </>
     )
